73a47c5416bb06b70134b7c11df6ce49ee88c6f1
Gilbmet <gilbmet254@gmail.com>
2022-09-08 08:47:40 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the string of the function
+ *
+ * description using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
