d80ffa7ceb63b5efa5b0341ec87a9e616adf5d00
Gilbmet <gilbmet254@gmail.com>
2022-09-08 15:34:36 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,8 @@
 
 
 /** 
- * main - prints the function
- *
- * Return: 0 if exited properly, non-zero otherwise  
+ *  main - prints the function
+ *  Return: 0 if exited properly, non-zero otherwise  
  */
 int main(void)
 {
