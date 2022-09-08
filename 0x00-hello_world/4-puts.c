d71323a93f55572eaec3aa0d184f443162807403
Gilbmet <gilbmet254@gmail.com>
2022-09-08 15:21:49 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
+
 /** 
- * main - prints the phrase "Programming is like building a multilingual puzzle
+ * main - prints the function
  *
  * Return: 0 if exited properly, non-zero otherwise  
  */
