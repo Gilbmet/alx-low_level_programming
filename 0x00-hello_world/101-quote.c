cd4a58da4d6ebebb8167f33a8dcacb45f2f00690
Gilbmet <gilbmet254@gmail.com>
2022-09-08 16:39:52 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 
 
 /**
- *  main - prints a text
- *  Return: Always 1
- */
+*  main - prints a text
+*  Return: Always 1
+*/
 int main(void)
 {
-	write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
-
