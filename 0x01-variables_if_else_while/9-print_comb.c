5a7492baa4cfd1d6ed97d06e42a46a88f458773b
Gilbmet <gilbmet254@gmail.com>
2022-09-09 15:14:41 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,19 +7,19 @@
  */
 int main(void)
 {
-		int i = 48;
-		/* your code goes there */
-  		while (i <= 57)
-		{
-			putchar(i);
-			if (i != 57)
-			{	
-				putchar(',');
-				putchar(' ');
-			}
-			i++;
-		}
-		putchar('\n');
-		return (0);
+int i = 48;
+/* your code goes there */
+while (i <= 57)
+{
+putchar(i);
+if (i != 57)
+{	
+putchar(',');
+putchar(' ');
+}
+i++;
+}
+putchar('\n');
+return (0);
 }
 
