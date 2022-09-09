7f7e904db6cc21b31e0a3bf3c0277de5debf943e
Gilbmet <gilbmet254@gmail.com>
2022-09-08 23:46:07 -0700

    Task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+*  main - entry point
+*  Return: always 0 (Success)
+*/
+int main(void)
+{
+int n;
+
+strand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n == 0)
+{
+printf("%i is zero\n", n);
+}
+
+else if (n < 0)
+{
+printf("%i is negative\n", n);
+}
+
+else
+{
+printf("%i is positive\n", n);
+}
+
+return (0);
+}
