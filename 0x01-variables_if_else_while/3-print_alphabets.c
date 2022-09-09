2d71d3de5207b0c54a470f9defca84a28bb85b9b
Gilbmet <gilbmet254@gmail.com>
2022-09-09 14:45:27 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,22 +1,43 @@
-#include <stdio.h>
-/**
-* main - print if the number is zero, positive or negative
-*
-* Description: using the function
-* This program prints "Programming is positive, zero or negative
-* Return: 0
-*/
-int main(void)
-{
-char ch;
-for (ch = 'a' ; ch = 'z' ; ch++)
-{
-	putchar(ch);
-}
-for (ch = 'A' ; ch <= 'Z' ; ch++)
-{
-	putchar(ch);
-}
-putchar('\n');
-return (0);
-}
+#include <stdio.h>                                                                                                                 
+
+/**                                                                                                                                
+
+* main - print if the number is zero, positive or negative                                                                         
+
+*                                                                                                                                  
+
+* Description: using the function                                                                                                  
+
+* This program prints "Programming is positive, zero or negative                                                                   
+
+* Return: 0                                                                                                                        
+
+*/                                                                                                                                 
+
+int main(void)                                                                                                                     
+
+{                                                                                                                                  
+
+char ch;                                                                                                                           
+
+for (ch = 'a' ; ch = 'z' ; ch++)                                                                                                   
+
+{                                                                                                                                  
+
+        putchar(ch);                                                                                                               
+
+}                                                                                                                                  
+
+for (ch = 'A' ; ch <= 'Z' ; ch++)                                                                                                  
+
+{                                                                                                                                  
+
+        putchar(ch);                                                                                                               
+
+}                                                                                                                                  
+
+putchar('\n');                                                                                                                     
+
+return (0);                                                                                                                        
+
+}       
