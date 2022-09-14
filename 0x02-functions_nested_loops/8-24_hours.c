a29a10e03972e8d631aebc4eebde07aaf74d9207
Gilbmet <gilbmet254@gmail.com>
2022-09-14 05:18:44 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,7 +16,7 @@ void jack_bauer(void)
 	{
 	while (minutes <= 59)
 	{
-av
+	mins_remainder = minutes % 10;
 	hours_remainder = hours % 10;
 	_putchar(hours / 10 + '0');
 	_putchar(hours_remainder + '0');
@@ -24,6 +24,9 @@ av
 	_putchar(minutes / 10 + '0');
 	_putchar(mins_remainder + '0');
 	minutes++;
+	_putchar('\n');
+	}
+	hours++;
 	minutes = 0;
 }
 }
