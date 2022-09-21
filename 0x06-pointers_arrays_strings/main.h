57966f7bdfd252e5b2ddb748fcd9d76cf7cc8d51
Gilbmet <gilbmet254@gmail.com>
2022-09-21 16:15:10 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * file: main.h
+ * Auth: Gilbmet
+ * Description: header file containing prototypes.
+ */
+int _putchar(char c);
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_number(int n);
+char *rot13(char *);
+char *leet(char *);
+char *cap_string(char *);
+char *string_toupper(char *);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+
+#endif /*MAIN_H*/
