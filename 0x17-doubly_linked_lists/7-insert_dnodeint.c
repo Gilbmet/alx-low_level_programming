a2e43ed0949fc4201956c0433c868ef1e4a47e2f
Gilbmet <gilbmet254@gmail.com>
2022-12-08 23:56:33 +0300

    7-insert_dnodeint.c
    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,55 +1,58 @@
 #include "lists.h"
+/**
+ * dlistint_len - returns the number of elements in a linked list
+ * @h: pointer to head of a list
+ * Return: number of elements in a linked list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+	const dlistint_t *temp = h;
+
+	while (temp)
+	{
+		count++;
+		temp = temp->next;
+	}
+	return (count);
+}
 
 /**
- * insert_dnodeint_at_index - inserts a new node at
- * a given position
- *
- * @h: head of the list
- * @idx: index of the new node
- * @n: value of the new node
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: double pointer to the head of a linked list
+ * @idx: the position where the new node should be entered
+ * @n: the data to put into the new node
  * Return: the address of the new node, or NULL if it failed
  */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new;
-	dlistint_t *head;
-	unsigned int i;
+	dlistint_t *temp = *h;
+	unsigned int counter = 0;
+	size_t length = dlistint_len(*h);
 
-	new = NULL;
+	if (idx > length)
+		return (NULL);
 	if (idx == 0)
-		new = add_dnodeint(h, n);
-	else
+		return (add_dnodeint(h, n));
+	if (length == idx)
+		return (add_dnodeint_end(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	while (counter < idx - 1)
 	{
-		head = *h;
-		i = 1;
-		if (head != NULL)
-			while (head->prev != NULL)
-				head = head->prev;
-		while (head != NULL)
-		{
-			if (i == idx)
-			{
-				if (head->next == NULL)
-					new = add_dnodeint_end(h, n);
-				else
-				{
-					new = malloc(sizeof(dlistint_t));
-					if (new != NULL)
-					{
-						new->n = n;
-						new->next = head->next;
-						new->prev = head;
-						head->next->prev = new;
-						head->next = new;
-					}
-				}
-				break;
-			}
-			head = head->next;
-			i++;
-		}
+		temp = temp->next;
+		counter++;
 	}
 
+	new->n = n;
+	new->next = temp->next;
+	new->prev = temp;
+	temp->next->prev = new;
+	temp->next = new;
 	return (new);
 }
- 
