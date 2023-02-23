047902e8054d7cd384e2c5324d068fc88508810e
Gilbmet <gilbmet254@gmail.com>
2023-02-23 13:22:58 +0300

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
-"""Defines an island perimeter measuring function."""
-
 
 def island_perimeter(grid):
-    """Return the perimiter of an island.
-    
-    The grid represents water by 0 and land by 1.
-    
-    Args:
-        grid (list): A list of list of integers representing an island.
-    Returns:
-        The perimeter of the island defined in grid.
     """
-    width = len(grid[0])
-    height = len(grid)
-    edges = 0
-    size = 0
+    Returns the perimeter of the island described in grid.
 
-    for i in range(height):
-        for j in range(width):
+    Args:
+        grid (list): a list of list of integers representing the island and water zones.
+
+    Returns:
+        int: the perimeter of the island.
+
+    """
+    rows, cols = len(grid), len(grid[0])
+    perimeter = 0
+    
+    for i in range(rows):
+        for j in range(cols):
             if grid[i][j] == 1:
-                size += 1
-                if (j > 0 and grid[i][j - 1] == 1):
-                    edges += 1
-                if (i > 0 and grid[i - 1][j] == 1):
-                    edges += 1
-    return size * 4 - edges * 2
+                # add 4 for a land cell
+                perimeter += 4
+                # subtract 2 for each adjacent land cell (up and left)
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
+    
+    return perimeter
